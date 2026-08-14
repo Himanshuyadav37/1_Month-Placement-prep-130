d4981a6cd91831a3a87ef00ffa1f6eb195bbd585
Himanshu <ydvhimanshu461@gmail.com>
2026-08-14 16:19:26 +0530

    pivot index done

--- Advance Array/src/PivotIndex.java
+++ Advance Array/src/PivotIndex.java
@@ -1,2 +1,42 @@
-public class PivotIndex {
+class PivotIndex {
+
+    public static int pivotIndex(int[] nums) {
+
+        // Step 1: Calculate total sum
+        int totalSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
+        }
+
+        // Step 2: Maintain left sum
+        int leftSum = 0;
+
+        // Step 3: Find pivot index
+        for (int i = 0; i < nums.length; i++) {
+
+            // Right sum = total - left - current element
+            int rightSum = totalSum - leftSum - nums[i];
+
+            // Check pivot
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            // Current element becomes part of left side
+            leftSum += nums[i];
+        }
+
+        // No pivot found
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 7, 3, 6, 5, 6};
+
+        int result = pivotIndex(nums);
+
+        System.out.println(result);
+    }
 }
