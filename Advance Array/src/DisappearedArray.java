d73353dfacd41ec54157e6c66f559cc0d77e1bfd
Himanshu <ydvhimanshu461@gmail.com>
2026-08-14 12:03:49 +0530

    Done Merge two sorted array

--- Advance Array/src/DisappearedArray.java
+++ Advance Array/src/DisappearedArray.java
@@ -1,2 +1,35 @@
-public class DisappearedArray {
+import java.util.*;
+
+class DisappearedArray {
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+
+        List<Integer> ans = new ArrayList<>();
+
+       // step 1: Mark present number
+        for(int i =0;i<nums.length;i++){
+            int index  = Math.abs(nums[i]-1);
+
+            if(nums[index]>0){
+                nums[index] = -nums[index];
+            }
+        }
+
+        // Step 2: FInd unmarked indexes
+        for(int i =0; i< nums.length; i++){
+            if(nums[i] > 0){
+                ans.add(i+1);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+
+        List<Integer> result = findDisappearedNumbers(nums);
+
+        System.out.println(result);
+    }
 }
--- Advance Array/src/MergeSortedArray.java
+++ Advance Array/src/MergeSortedArray.java
@@ -1,2 +1,48 @@
-public class MergeSortedArray {
+import java.util.*;
+
+class MergeSortedArray {
+
+    public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
+
+        int m = arr1.length;
+        int n = arr2.length;
+
+        int[] ans = new int[m + n];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        // Compare both arrays
+        while (i < m && j < n) {
+
+            if (arr1[i] <= arr2[j]) {
+                ans[k++] = arr1[i++];
+            } else {
+                ans[k++] = arr2[j++];
+            }
+        }
+
+        // Remaining elements of arr1
+        while (i < m) {
+            ans[k++] = arr1[i++];
+        }
+
+        // Remaining elements of arr2
+        while (j < n) {
+            ans[k++] = arr2[j++];
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+
+        int[] result = mergeSortedArrays(arr1, arr2);
+
+        System.out.println(Arrays.toString(result));
+    }
 }
