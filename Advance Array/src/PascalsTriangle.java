42e4b9a62def8a8de9f28ffc40186d53a952689c
Himanshu <ydvhimanshu461@gmail.com>
2026-08-15 09:21:09 +0530

    Advance array complete (10 problems solved)

--- Advance Array/src/PascalsTriangle.java
+++ Advance Array/src/PascalsTriangle.java
@@ -1,2 +1,44 @@
-public class PascalsTriangle {
+import java.util.*;
+
+class PascalsTriangle {
+
+    public static List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+
+                if (j == 0 || j == i) {
+
+                    row.add(1);
+
+                } else {
+
+                    int value =
+                            triangle.get(i - 1).get(j - 1)
+                                    +
+                                    triangle.get(i - 1).get(j);
+
+                    row.add(value);
+                }
+            }
+
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+
+        int numRows = 5;
+
+        List<List<Integer>> result = generate(numRows);
+
+        System.out.println(result);
+    }
 }
